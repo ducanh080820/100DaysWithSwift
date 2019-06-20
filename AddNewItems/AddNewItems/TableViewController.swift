5f33d202d2c995234c846aef6d47d94bdc69376b
Tranducanh <ducanhtran080820@gmail.com>
2019-06-20 20:04:11 +0700

    add new items

---
+++ AddNewItems/AddNewItems/TableViewController.swift
@@ -0,0 +1,53 @@
+//
+//  TableViewController.swift
+//  AddNewItems
+//
+//  Created by Tranducanh on 6/19/19.
+//  Copyright © 2019 Tranducanh. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+
+    var arrData = ["apple", "ham", "milk", "egg"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationItem.leftBarButtonItem = self.editButtonItem
+    }
+    
+    // MARK: - Table view data source
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return arrData.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        cell.textLabel?.text = arrData[indexPath.row]
+        cell.accessoryType = .disclosureIndicator
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        //        let rowToMove = arrData[sourceIndexPath.row]
+        //        arrData.remove(at: sourceIndexPath.row)
+        //        arrData.insert(rowToMove, at: sourceIndexPath.row)
+    }
+    
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            arrData.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }
+    }
+
+}
