bbf8bedb462a211c65ed0b37cf71379a2515aa6c
DSCO <ducanhtran080820@gmail.com>
2019-06-26 15:36:47 +0700

    delay animation

---
+++ CoreAnimations/CoreAnimations/ViewController.swift
@@ -0,0 +1,63 @@
+//
+//  ViewController.swift
+//  CoreAnimations
+//
+//  Created by DSCO on 6/26/19.
+//  Copyright © 2019 DSCO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var imageView: UIImageView!
+    
+    var currentAnimation = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        imageView = UIImageView(image: UIImage(named: "penguin"))
+        imageView.center = view.center
+        view.addSubview(imageView)
+    }
+    
+    @IBAction func tapped(_ sender: UIButton) {
+        sender.isHidden = true
+        
+        UIView.animate(withDuration: 5, delay: 0, options: [], animations: {
+            switch self.currentAnimation {
+            case 0:
+                self.imageView.transform = CGAffineTransform(scaleX: 2, y: 2)
+            case 1:
+                self.imageView.transform = .identity
+            case 2:
+                self.imageView.transform = CGAffineTransform(translationX: -256, y: -256)
+            case 3:
+                self.imageView.transform = .identity
+            case 4:
+                self.imageView.transform = CGAffineTransform(rotationAngle: .pi)
+            case 5:
+                self.imageView.transform = .identity
+            case 6:
+                self.imageView.alpha = 0.1
+                self.imageView.backgroundColor = .green
+            case 7:
+                self.imageView.alpha = 1
+                self.imageView.backgroundColor = .clear
+            default:
+                break
+            }
+        }) { finished in
+            sender.isHidden = false
+        }
+        
+        currentAnimation += 1
+        
+        if currentAnimation > 7 {
+            currentAnimation = 0
+        }
+    }
+
+}
+
