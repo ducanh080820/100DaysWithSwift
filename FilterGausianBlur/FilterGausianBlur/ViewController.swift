27ca67ac4cef6abb7523289c54ba2f9ca46803f8
DSCO <ducanhtran080820@gmail.com>
2019-06-28 10:18:00 +0700

    FilterGaussianBlur

---
+++ FilterGausianBlur/FilterGausianBlur/ViewController.swift
@@ -0,0 +1,65 @@
+//
+//  ViewController.swift
+//  FilterGausianBlur
+//
+//  Created by DSCO on 6/28/19.
+//  Copyright © 2019 DSCO. All rights reserved.
+//
+
+import UIKit
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var label: UILabel!
+    
+    var image: UIImage = UIImage(named: "face")!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        imageView.image = self.image
+        label.text = ""
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    
+    @IBAction func changeValue(_ sender: AnyObject) {
+        
+        let slider = sender as! UISlider
+        let value: Float = slider.value
+        
+        self.filterGaussianBlur(value)
+    }
+    
+    //MARK: 高斯模糊滤镜效果
+    func filterGaussianBlur(_ value: Float) {
+        
+        let context = CIContext(options: nil)
+        let cImage = CIImage(cgImage: self.image.cgImage!)
+        
+        let guassianBlur = CIFilter(name: "CIGaussianBlur")
+        guassianBlur?.setValue(cImage, forKey: "inputImage")
+        
+        let text = String(format: "高斯模糊 Radius : % .2f", value * 10)
+        self.label.text = text
+        
+        guassianBlur!.setValue(value, forKey: "inputRadius")
+        let result = guassianBlur?.value(forKey: "outputImage") as! CIImage
+        
+        let imageRef = context.createCGImage(result, from: CGRect(x: 0, y: 0, width: self.imageView.image!.size.width, height: self.imageView.image!.size.height))
+        
+        let image = UIImage(cgImage: imageRef!)
+        self.imageView.image = image
+        
+    }
+    
+    
+    
+}
